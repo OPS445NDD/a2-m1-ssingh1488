65ec010b84c1ce62fb0970e13dbdac2deaac5828
Satnam Singh <ssingh1488@myseneca.ca>
2026-07-17 16:57:15 -0400

    Complete Assignment 2 milestone 1 memory functions

--- assignment2.py
+++ assignment2.py
@@ -1,75 +1,35 @@
-#!/usr/bin/env python3
+def percent_to_graph(percent, total_chars):
+    """Convert a percentage between 0.0 and 1.0 into a bar graph."""
+    hash_count = int(percent * total_chars)
+    space_count = total_chars - hash_count
 
-'''
-OPS445 Assignment 2 - Winter 2023
-Program: assignment2.py 
-Author: "Student Name"
-The python code in this file is original work written by
-"Student Name". No code in this file is copied from any other source 
-except those provided by the course instructor, including any person, 
-textbook, or on-line resource. I have not shared this python script 
-with anyone or anything except for submission for grading.  
-I understand that the Academic Honesty Policy will be enforced and 
-violators will be reported and appropriate action will be taken.
+    return "#" * hash_count + " " * space_count
 
-Description: <Enter your documentation here>
 
-Date: 
+def get_sys_mem():
+    """Return total system memory in kilobytes."""
+    with open("/proc/meminfo", "r") as mem_file:
+        for line in mem_file:
+            if line.startswith("MemTotal:"):
+                return int(line.split()[1])
 
-'''
+    return 0
 
-import argparse
-import os, sys
 
-def parse_command_args() -> object:
-    "Set up argparse here. Call this function inside main."
-    parser = argparse.ArgumentParser(description="Memory Visualiser -- See Memory Usage Report with bar charts",epilog="Copyright 2023")
-    parser.add_argument("-l", "--length", type=int, default=20, help="Specify the length of the graph. Default is 20.")
-    # Create an entry for human-readable. Check the docs to make it a True/False option.
-    parser.add_argument("program", type=str, nargs='?', help="if a program is specified, show memory use of all associated processes. Show only total use if not.")
-    args = parser.parse_args()
-    return args
+def get_avail_mem():
+    """Return available system memory in kilobytes."""
+    mem_free = 0
+    swap_free = 0
 
-def percent_to_graph(percent: float, length: int=20) -> str:
-    "turns a percent 0.0 - 1.0 into a bar graph"
-    pass
+    with open("/proc/meminfo", "r") as mem_file:
+        for line in mem_file:
+            if line.startswith("MemAvailable:"):
+                return int(line.split()[1])
 
-def get_sys_mem() -> int:
-    "return total system memory (used or available) in kB"
-    # open the meminfo file to do this!
-    pass
+            if line.startswith("MemFree:"):
+                mem_free = int(line.split()[1])
 
-def get_avail_mem() -> int:
-    "return total memory that is currently available"
-    # open the meminfo file to do this!
-    pass
+            if line.startswith("SwapFree:"):
+                swap_free = int(line.split()[1])
 
-def pids_of_prog(app_name: str) -> list:
-    "given an app name, return all pids associated with app"
-    # please use os.popen('pidof <app>') to do this!
-    pass
-
-def rss_mem_of_pid(proc_id: str) -> int:
-    "given a process id, return the Resident memory used"
-    # for a process, open the smaps file and return the total of each
-    # Rss line.
-    pass
-
-def bytes_to_human_r(kibibytes: int, decimal_places: int=2) -> str:
-    "turn 1,024 into 1 MiB, for example"
-    suffixes = ['KiB', 'MiB', 'GiB', 'TiB', 'PiB']  # iB indicates 1024
-    suf_count = 0
-    result = kibibytes 
-    while result > 1024 and suf_count < len(suffixes):
-        result /= 1024
-        suf_count += 1
-    str_result = f'{result:.{decimal_places}f} '
-    str_result += suffixes[suf_count]
-    return str_result
-
-if __name__ == "__main__":
-    args = parse_command_args()
-    if not args.program:  # not program name is specified.
-        pass
-    else:
-        pass
+    return mem_free + swap_free
